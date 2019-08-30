966869308a8fea1708d2b44b6abde283773d23d4
Ben Chaimo <doroza@hotmail.com>
2019-08-31 01:13:41 +0300

    first laying out

--- remind-client/src/app/app.module.ts
+++ remind-client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MylistComponent } from './components/mylist/mylist.component';
+import { MyfriendsComponent } from './components/myfriends/myfriends.component';
+import { MyscoreComponent } from './components/myscore/myscore.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MylistComponent,
+    MyfriendsComponent,
+    MyscoreComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
