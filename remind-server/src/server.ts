e957dc5530f59f9fb006e533b571c659e2ba3e56
Ben Chaimo <doroza@hotmail.com>
2019-09-15 22:56:29 +0300

    still determining best course of action

--- remind-server/src/server.ts
+++ remind-server/src/server.ts
@@ -2,9 +2,10 @@ import http from 'http';
 import express from 'express';
 
 const router = express();
+
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
 
 server.listen(PORT, () => {
     console.log(`Remind Server is listening at http://localhost:${PORT}...`);
-})
+});
