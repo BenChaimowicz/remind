44c5afa23b4616f3d4f9c8f98a99544ef6fa8afc
Ben Chaimo <doroza@hotmail.com>
2019-09-16 21:13:24 +0300

    basic middlware

--- remind-server/src/server.ts
+++ remind-server/src/server.ts
@@ -1,7 +1,12 @@
 import http from 'http';
 import express from 'express';
+import { loggerMiddleware } from './middleware/logger.middleware';
+import bodyParser from 'body-parser';
 
 const router = express();
+router.use(loggerMiddleware);
+router.use(bodyParser.json());
+
 
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
