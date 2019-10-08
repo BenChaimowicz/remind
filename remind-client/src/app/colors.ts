71895adefaa70de7bc361dfd2d395fbab1c8c6eb
Ben Chaimo <doroza@hotmail.com>
2019-10-08 23:09:30 +0300

    added color scheme

---
+++ remind-client/src/app/colors.ts
@@ -0,0 +1,7 @@
+export class Colors {
+  static onyx = '#343A40';
+  static jellybean = '#DB504A';
+  static citrine = '#E3B505';
+  static paynesgrey = '#4F6D7A';
+  static cadetblue = '#56A3A6';
+}
