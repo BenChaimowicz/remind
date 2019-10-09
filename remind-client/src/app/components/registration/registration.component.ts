9a6e2089f81a7f0d54e347ceaabae40b6f687aac
Ben Chaimo <doroza@hotmail.com>
2019-10-09 03:18:43 +0300

    reg form validation

--- remind-client/src/app/components/registration/registration.component.ts
+++ remind-client/src/app/components/registration/registration.component.ts
@@ -15,11 +15,10 @@ import { NgbDateCustomParserFormatter } from '../../adapters/dateformat.adapter'
 export class RegistrationComponent implements OnInit {
 
   date: NgbDateStruct;
-  public today = `${this.calendar.getToday().day}-${this.calendar.getToday().month}-${this.calendar.getToday().year}`;
   public disclaimer = `Remind is a web application designed to help you commit to better handling your
   time while also thinking of cool new ways to spend it, for you and others.
   By sharing your daily tasks with others, and getting points while achieving them, Remind aims to make everyone
-  more effective and driven towards one's goals.`;
+  more effective and driven towards one's goals. Please note that all fields are required!`;
   public regForm: FormGroup = new FormGroup({
     emailInput: new FormControl('', [Validators.required, Validators.email]),
     userNameInput: new FormControl('', [Validators.required, Validators.pattern(RegistrationValidators.userNamePattern)]),
@@ -27,7 +26,7 @@ export class RegistrationComponent implements OnInit {
       passwordInput: new FormControl('', [Validators.required, RegistrationValidators.cannotContainSpace]),
       passwordVerify: new FormControl('', [Validators.required])
     }, [Validators.required, RegistrationValidators.mustBeSameAs]),
-    birthdayInput: new FormControl(this.today, [Validators.required])
+    birthdayInput: new FormControl(moment(), [Validators.required])
   });
 
   constructor(private loginService: LoginService, public calendar: NgbCalendar) {
@@ -37,6 +36,5 @@ export class RegistrationComponent implements OnInit {
   }
 
   public async register() {
-
   }
 }
