bb0192284004aef6819cdc3183bdb609d730ef5d
Ben Chaimo <doroza@hotmail.com>
2019-10-15 03:12:33 +0300

    animations

--- remind-client/src/app/components/mylist/mylist.component.ts
+++ remind-client/src/app/components/mylist/mylist.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-mylist',
   templateUrl: './mylist.component.html',
-  styleUrls: ['./mylist.component.css']
+  styleUrls: ['./mylist.component.css'],
+  animations: [
+    trigger('throwIn', [
+      transition('void => *', [
+        style({ transform: 'translateX(-10%)', width: '0' }),
+        animate('500ms ease')]),
+      transition('* => void', [
+        style({ opacity: 0 }),
+        animate('200ms ease')])
+    ])
+  ]
 })
 export class MylistComponent implements OnInit {
+
+  showInput = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleShowInput() {
+    this.showInput = !this.showInput;
   }
 
 }
--- remind-client/src/app/services/lists-service.service.ts
+++ remind-client/src/app/services/lists-service.service.ts
@@ -13,7 +13,7 @@ export class ListsServiceService {
       { id: 1, name: 'remind1', reward: 10 },
       { id: 2, name: 'remind2', reward: 10 },
       { id: 3, name: 'remind3', reward: 10 },
-      { id: 4, name: 'remind4', reward: 10 }
+      { id: 4, name: 'remind4', reward: 10 },
     ]
   };
 
